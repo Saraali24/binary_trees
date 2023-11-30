20d758bef5d776f5d024ee9bef389b08a6e90219
Saraali24 <134275397+Saraali24@users.noreply.github.com>
2023-11-30 21:33:32 +0200

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,10 +1,12 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_nodes - Counts the nodes with 1 child or more.
+ * binary_tree_nodes - function that counts the nodes with 1 child or more.
  * @tree: ptr to the tree root.
  *
  * Return: the Cnodes count.
  */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t Cnodes = 0;
